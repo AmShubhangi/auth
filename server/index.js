fb590b4d9002c5ff6737db99a04ee1b7417b0772
Shubhangi Ambade <chirag.jakhariya@rokigroup.co.uk>
2019-12-23 18:55:57 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -4,10 +4,16 @@ const http = require('http');
 const bodyParse = require('body-parser');
 const morgan = require('morgan');
 const app = express();
+const router = require('./router')
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth')
 
 // App Setup
 app.use(morgan('combined'));
 app.use(bodyParse.json({ type: '*/*' }));
+router(app);
 
 // Server setUp
 const port = process.env.PORT || 3090;
